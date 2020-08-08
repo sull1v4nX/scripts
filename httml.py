03b3e26adac6559b8303c5bc16f02feaf1d0b113
Sull1v4nX <sull1v4n.x@gmail.com>
2020-08-08 09:11:22 -0500

    Ajuste mensaje salida

--- httml.py
+++ httml.py
@@ -7,7 +7,6 @@ Created on Wed Jul  1 18:38:55 2020
 @author: Sull1v4nX 
 """
 
-
 import difflib
 file1 = open('original.txt', 'r').readlines()
 file2 = open('site2.txt','r').readlines()
@@ -18,5 +17,5 @@ htmldiffs = htmlDiffer.make_file(file1, file2)
 with open('comparison.html', 'w') as outfile:
     outfile.write(htmldiffs)
     
-print("Fin del programar")
+print("Fin del programa")
 
